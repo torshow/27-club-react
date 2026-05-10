6130f22cf92df574c249412e1523df6b1e15f442
torshow <slywoods12@gmail.com>
2026-05-10 23:28:46 +0300

    add useState

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, {useState } from "react";
 
 function SearchBar() {
+  const [text, setText] = useState("")
   return (
     <div className="searchbar">
       <input
         type="text"
         placeholder="Search artist..."
         className="searchbar-input"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
       />
       <button className="searchbar-btn">Search</button>
     </div>
