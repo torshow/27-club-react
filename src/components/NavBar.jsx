046ea0ba034ef3157b75efa0a62d797a6c3f57fb
torshow <slywoods12@gmail.com>
2026-05-13 10:08:47 +0300

    update navbar links and logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,9 +3,10 @@ import React from "react";
 function NavBar() {
   return (
     <nav className="nav">
-      <div className="logo">27 club showcase</div>
+      <div className="logo">Portfolio</div>
       <ul className="links">
         <li><a href="/">Home</a></li>
+        <li><a href="/projects">Projects</a></li>
         <li><a href="/about">About</a></li>
         <li><a href="/contact">Contact</a></li>
       </ul>
