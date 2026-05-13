f269c6320522dc4efbc05b566f09b73e4dab7cbf
torshow <slywoods12@gmail.com>
2026-05-13 10:13:44 +0300

    update artistlist to use projects prop

--- src/components/ArtistList.jsx
+++ src/components/ArtistList.jsx
@@ -1,33 +1,22 @@
 import React from "react";
 import ArtistCard from "./ArtistCard";
 
-
-function ArtistList() {
+function ArtistList({ projects }) {
   return (
     <div className="artist-list">
-      <h2>Music Legends</h2>
+      <h2>Projects</h2>
 
-      <div className="artist-card">
-        <h3>Kurt Cobain</h3>
-        <p>Band: Nirvana</p>
-        <p>Genre: Grunge</p>
-        <p>Died: 1994</p>
+      <div className="artist-cards">
+        {projects.map(project => (
+          <ArtistCard
+            key={project.id}
+            name={project.name}
+            band={project.client}
+            genre={project.category}
+            died={project.year}
+          />
+        ))}
       </div>
-
-      <div className="artist-card">
-        <h3>Amy Winehouse</h3>
-        <p>Band: Solo Artist</p>
-        <p>Genre: Soul / R&B</p>
-        <p>Died: 2011</p>
-      </div>
-
-      <div className="artist-card">
-        <h3>Jimi Hendrix</h3>
-        <p>Band: Jimi Hendrix Experience</p>
-        <p>Genre: Rock / Blues</p>
-        <p>Died: 1970</p>
-      </div>
-
     </div>
   );
 }
